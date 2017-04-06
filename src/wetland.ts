957a6180cdc40212461a1d3ae36f248407526a93
RWOverdijk <r.w.overdijk@gmail.com>
2017-04-06 16:58:14 +0200

    chore(project): remove unused command

--- src/wetland.ts
+++ src/wetland.ts
@@ -6,5 +6,5 @@ import {getVersion} from './helpers';
 program.version(getVersion())
   .command('migrator', 'Work with your migrations.')
   .command('snapshot', 'Work with mapping snapshots.')
-  .command('entity', 'Entity utilities.')
+  .command('generator', 'Work with multiple generators.')
   .parse(process.argv);
